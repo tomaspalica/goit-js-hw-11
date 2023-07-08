82a270dc447953359a2f018926e8e3b553e70138
tomaspalica <tomasss.palica@gmail.com>
2023-07-08 10:55:40 +0200

    s

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
+const searchForm = document.querySelector(".search-form")
+const searchInput = document.querySelector(".search-form input[name='searchQuery']")
+const searchButton = document.querySelector(".search-form button")
+const gallery = document.querySelector(".gallery")
+const loadMoreBtn = document.querySelector(".load-more")
+
+const myKey = "example";
+let q = searchInput.value
+let page = 1;
+searchInput.addEventListener("input", () => {
+    console.log(searchInput.value)
+})
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(searchInput.value)
+})
