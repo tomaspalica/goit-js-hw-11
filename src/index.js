6ba3aa63ba9d5ab8cb4590484a3e076fb12014ad
tomaspalica <tomasss.palica@gmail.com>
2023-07-08 14:30:35 +0200

    did some work with hw11

--- src/index.js
+++ src/index.js
@@ -1,18 +1,86 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 const searchForm = document.querySelector(".search-form")
 const searchInput = document.querySelector(".search-form input[name='searchQuery']")
 const searchButton = document.querySelector(".search-form button")
 const gallery = document.querySelector(".gallery")
 const loadMoreBtn = document.querySelector(".load-more")
 
-const myKey = "example";
-let q = searchInput.value
+const API_KEY = "38123861-5044a45b99316196e4506c2b7";
+
 let page = 1;
-searchInput.addEventListener("input", () => {
-    console.log(searchInput.value)
+
+
+const fetchPictures = async () => {
+    const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(searchInput.value)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+    if(response.data.totalHits === 0){
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+       }
+    const imgs = await response.data
+    return imgs 
+}
+
+searchForm.addEventListener("submit", (e) => {
+ e.preventDefault()
+ if(searchInput.value === ""){
+    Notiflix.Notify.failure("Fill in the input")
+    return
+ }
+ page= 1
+gallery.innerHTML = ""
+ fetchPictures()
+ .then(imgs => {
+    
+    console.log(imgs)
+    page += 1;
+    return renderImgList(imgs)})
+    .catch(error => console.error(error))
+ loadMoreBtn.style["visibility"] = "visible"
 })
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault()
-    console.log(searchInput.value)
+loadMoreBtn.addEventListener("click", () => {
+   
+    fetchPictures()
+ .then(imgs => {
+    if(page > imgs.totalHits/40){
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
+        return
+    }
+    console.log(imgs)
+    page += 1;
+    return renderImgList(imgs)})
+    .catch(error => console.error(error))
 })
+function renderImgList(imgs) {
+    const markup = imgs.hits
+      .map((img) => {
+        return `<div class="photo-card">
+        <a class="gallery__link" href="${img.largeImageURL}">
+        <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes:${img.likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views:${img.views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments:${img.comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads:${img.downloads}</b>
+          </p>
+        </div>
+      </div>`;
+      })
+      .join("");
+    gallery.insertAdjacentHTML("beforeend", markup);
+  }
+  const lightBox = new SimpleLightbox(".gallery a", {
+    captionType: "alt",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
